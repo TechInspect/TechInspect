c6f87775fd09c77dcde174b3955fd2f4557d2cbc
User <kd200681@mail.ru>
2021-06-28 16:51:16 +0300

    add views

--- car_park/views.py
+++ car_park/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 
-from .forms import BbForm
+from .forms import TechInspectForm
 from .models import Park, UserUID
 
 
 def index(request):
     park = Park.objects.all()
     user_uid = UserUID.objects.all()
-    context = {'bbs': park, 'rubrics': user_uid}
+    context = {'parks': park, 'user_uid': user_uid}
     return render(request, 'car_park/index.html', context)
 
+
+class ParkCreateView(CreateView):
+    template_name = 'car_park/create.html'
+    form_class = TechInspectForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['parks'] = Park.objects.all()
+        return context
