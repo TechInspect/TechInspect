7bbeaef577bd78dc8e8d480a9b4e70c56630f570
User <kd200681@mail.ru>
2021-06-28 15:37:01 +0300

    create model car park

---
+++ car_park/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+# Create your models here.
+class Park(models.Model):
+    uid = models.ForeignKey('UserUID', null=True, on_delete=models.PROTECT, verbose_name='ID пользователя')
+    brand = models.CharField(max_length=50, db_index=True, null=True, blank=True, verbose_name='Марка автомобиля')
+    model = models.CharField(max_length=50, db_index=True, null=True, blank=False, verbose_name='Модель автомобиля')
+    year = models.IntegerField(max_length=4, null=True, blank=True, verbose_name='Год производства автомобиля')
+    mileage = models.IntegerField(max_length=4, null=True, blank=True, verbose_name='Пробег автомобиля')
+    day_of_car_inspection = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='День осмотра авто')
+
+    class Meta:
+        verbose_name_plural = 'Парк автомобилей'
+        verbose_name = 'Автомобиль'
+        ordering = ['uid']
+
+
+class UserUID(models.Model):
+    user_uid = models.IntegerField(max_length=20, db_index=True, verbose_name='Пользователь')
+
+    def __str__(self):
+        """For read in admin"""
+        return self.user_uid
+
+    class Meta:
+        verbose_name_plural = 'Пользователи'
+        verbose_name = 'Пользователь'
+        ordering = ['user_uid']
