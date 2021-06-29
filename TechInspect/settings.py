9326e6087df46c72a1141a32fc944ad4331bf18c
Ilya <by_Ilusion@mail.ru>
2021-06-29 19:59:50 +0700

    Added html and static

--- TechInspect/settings.py
+++ TechInspect/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 env = environ.Env(
     # set casting, default value
@@ -46,7 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'car_park',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -128,6 +131,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if DEBUG:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ mainapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import re_path
+from mainapp import views as mainapp
+
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    re_path(r'^$', mainapp.index, name='index')
+]
